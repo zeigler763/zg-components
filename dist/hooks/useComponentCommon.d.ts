455db9b5f0fa421acee15ce705012e22a996c08a
yushimin <yushimin@qq.com>
2023-06-07 16:06:00 +0800

    更改忽略

--- dist/hooks/useComponentCommon.d.ts
+++
@@ -1,6 +0,0 @@
-import { TextComponentProps } from '../defaultProps';
-declare const useComponentCommon: (props: Readonly<Partial<TextComponentProps>>, picks: string[]) => {
-    styleProps: import("vue").ComputedRef<Partial<Readonly<Partial<TextComponentProps>>>>;
-    handleClick: () => void;
-};
-export default useComponentCommon;
